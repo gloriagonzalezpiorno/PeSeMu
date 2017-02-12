189cc18db0524a329608919c894288a64cbf326f
gloria <gloria@gloria-Lenovo-YOGA-700-14ISK>
2017-02-12 18:18:58 +0100

    añadidados estilos css

--- pesemu/src/main/java/dad/practica/pesemu/services/FacturaService.java
+++ pesemu/src/main/java/dad/practica/pesemu/services/FacturaService.java
@@ -1,5 +1,7 @@
 package dad.practica.pesemu.services;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,11 +15,19 @@ import dad.practica.pesemu.model.Factura;
 public class FacturaService {
 
 	public void crearFactura(CarritoCompra compra) {
+		//escritura de datos
 		try {
-			PrintWriter pw = new PrintWriter(new FileWriter("factura.txt"));
+			PrintWriter pw = new PrintWriter(new FileWriter("facturas/factura.txt"));
 			pw.print(new Factura(compra));
 			pw.flush();
 			pw.close();
+/*		//lectura de datos
+			BufferedReader br=new BufferedReader(new FileReader("\factura.txt"));
+			String s,s2=new String();
+			while((s=br.readLine())!=null)
+				s2+=s+"\n";
+			System.out.println("Texto leido:"+"\n"+s2);
+			br.close();*/
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
