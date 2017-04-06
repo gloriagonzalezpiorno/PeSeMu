159f148a0d2465a0ed586e38287873db8d59e363
gloria <gloria@gloria-Lenovo-YOGA-700-14ISK>
2017-04-06 12:20:26 +0200

    Añadido Hazelcast

--- pesemu/src/main/java/dad/practica/pesemu/PesemuApplication.java
+++ pesemu/src/main/java/dad/practica/pesemu/PesemuApplication.java
@@ -1,12 +1,30 @@
 package dad.practica.pesemu;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class PesemuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PesemuApplication.class, args);
 	}
+	
+	@Bean
+	 public Config config() {
+		Config config = new Config();
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+		return config;
+	 }
+	
 }
